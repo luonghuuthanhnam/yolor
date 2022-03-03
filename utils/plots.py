2d18fac542b5cfae614805f0e80e65d6d01e2b3d
Nam Luong <nam.luong@bizzi.vn>
2022-03-03 15:37:00 +0700

    fix error ' use Tensor.cpu()' when training end

--- utils/plots.py
+++ utils/plots.py
@@ -95,6 +95,7 @@ def output_to_target(output, width, height):
     for i, o in enumerate(output):
         if o is not None:
             for pred in o:
+                pred = pred.cpu()
                 box = pred[:4]
                 w = (box[2] - box[0]) / width
                 h = (box[3] - box[1]) / height
@@ -105,7 +106,7 @@ def output_to_target(output, width, height):
 
                 targets.append([i, cls, x, y, w, h, conf])
 
-    return np.array(targets)
+    return np.array(targets.cpu())
 
 
 def plot_images(images, targets, paths=None, fname='images.jpg', names=None, max_size=640, max_subplots=16):
